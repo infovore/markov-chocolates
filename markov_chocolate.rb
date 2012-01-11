8fb44965a85c5ccbc7b89ecbd8a5ebaaf34d5bc6
Tom Armitage <tom@infovore.org>
2012-01-11 18:35:06 +0000

    Adding Twitter command.

--- markov_chocolate.rb
+++ markov_chocolate.rb
@@ -7,12 +7,20 @@ require "lib/markov_chain.rb"
 
 Bundler.require(:default)
 
-#PATH_PREFIX = File.expand_path(File.dirname(__FILE__))
-#config = YAML.parse(File.read(PATH_PREFIX + "/creds.yml"))
+PATH_PREFIX = File.expand_path(File.dirname(__FILE__))
+config = YAML.parse(File.read(PATH_PREFIX + "/creds.yml"))
 
-#%w{consumer_key consumer_secret access_token access_token_secret}.each do |key|
-  #Object.const_set(key.upcase, config["config"][key].value)
-#end
+%w{consumer_key consumer_secret access_token access_token_secret}.each do |key|
+  Object.const_set(key.upcase, config["config"][key].value)
+end
+
+Twitter.configure do |config|
+  config.consumer_key = CONSUMER_KEY
+  config.consumer_secret = CONSUMER_SECRET
+  config.oauth_token = ACCESS_TOKEN
+  config.oauth_token_secret = ACCESS_TOKEN_SECRET
+end
+
 
 mc = MarkovChain.new(File.read("chocolate.txt"))
 # one in four times, generate a chain
@@ -23,5 +31,5 @@ if(rand(4) < 1)
   while string.size > 140
     string = mc.sentences(1).capitalize
   end
-  # tweet it
+  Twitter.update(string)
 end
