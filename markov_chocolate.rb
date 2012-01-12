7bf390c995551888ffb6a09c1ea059840bc00c1c
Tom Armitage <tom@infovore.org>
2012-01-12 09:43:06 +0000

    Tweaking randomness - should be run every minute.

--- markov_chocolate.rb
+++ markov_chocolate.rb
@@ -2,10 +2,10 @@
 
 require "rubygems"
 require "bundler/setup"
+Bundler.require(:default)
 require "#{File.dirname(__FILE__)}/lib/array.rb"
 require "#{File.dirname(__FILE__)}/lib/markov_chain.rb"
 
-Bundler.require(:default)
 
 PATH_PREFIX = File.expand_path(File.dirname(__FILE__))
 config = YAML.parse(File.read(PATH_PREFIX + "/creds.yml"))
@@ -21,15 +21,15 @@ Twitter.configure do |config|
   config.oauth_token_secret = ACCESS_TOKEN_SECRET
 end
 
-
 mc = MarkovChain.new(File.read("chocolate.txt"))
-# one in four times, generate a chain
-if(rand(4) < 1)
+# one in 120 times, generate a chain
+if(rand(120) < 1)
 # if it's less than 141 chars, tweet it
   string = mc.sentences(1).capitalize
 # otherwise re-generate until it fits
   while string.size > 140
     string = mc.sentences(1).capitalize
   end
+  puts "posting tweet #{string}"
   Twitter.update(string)
 end
